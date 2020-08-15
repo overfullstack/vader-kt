4cf89e323e5dca7caa7d4efea8d6f4da3d248a47
Gopal S Akshintala <gopalakshintala@gmail.com>
2020-08-15 10:50:01 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("org.sonarqube") version "3.0"
+    id("io.gitlab.arturbosch.detekt") version "1.11.0"
+    id("com.adarshr.test-logger") version "2.1.0"
+}
+
+group = "io.overfullstack"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    maven("https://oss.jfrog.org/artifactory/oss-snapshot-local/")
+    jcenter()
+}
+
+val arrowSnapshotVersion = "latest.integration"
+
+dependencies {
+    implementation("io.arrow-kt:arrow-core:$arrowSnapshotVersion")
+    implementation("io.arrow-kt:arrow-fx:$arrowSnapshotVersion")
+    implementation("io.arrow-kt:arrow-fx-coroutines:$arrowSnapshotVersion")
+    implementation("io.github.microutils:kotlin-logging:+")
+    // All other dependencies for log4j2 are taken care by kotlin-logging, so you don't need the below
+    // implementation("org.slf4j:slf4j-api:+")
+    runtimeOnly("org.apache.logging.log4j:log4j-slf4j18-impl:+") // slf4j18 - 18 is required for newer version.
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:+")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:+")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_14.toString()
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform {
+        excludeEngines("junit-vintage")
+    }
+}
+
+testlogger {
+    setTheme("mocha")
+    showExceptions = true
+    showStackTraces = true
+    showFullStackTraces = true
+    showCauses = true
+    slowThreshold = 2000
+    showSummary = true
+    showSimpleNames = true
+    showPassed = true
+    showSkipped = true
+    showFailed = true
+    showStandardStreams = true
+    showPassedStandardStreams = true
+    showSkippedStandardStreams = true
+    showFailedStandardStreams = true
+}
+
+detekt {
+    baseline = file("${rootProject.projectDir}/config/baseline.xml")
+    config = files("config/detekt/detekt.yml")
+    buildUponDefaultConfig = true
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal() // This is for other community plugins
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+        maven("https://dl.bintray.com/kotlin/kotlin-dev")
+    }
+    val kotlinEap: String by settings
+    plugins {
+        kotlin("jvm") version kotlinEap // This is handy if there are multiple modules. This lets you declare version at one place.
+    }
+}
+
+rootProject.name = "vader-kt"
+
